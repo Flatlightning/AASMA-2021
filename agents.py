55a51d4608590d79882e8292a8052fad5f3fd0da
Duarte Miranda <43819714+Flatlightning@users.noreply.github.com>
2021-05-25 23:07:31 +0100

    fix quadrant decision

--- agents.py
+++ agents.py
@@ -75,15 +75,15 @@ class Taxi(Agent):
     def calculate_quadrants(self):
         quadrant_area = self.board.board_size[0] * self.board.board_size[1]
         count = 0
-        while quadrant_area % 4:
+        while not quadrant_area % 4:
             quadrant_area = quadrant_area // 4
             count += 2
-        self.quadrant_size = math.sqrt(quadrant_area)
-        pos_x = 0
+        self.quadrant_size = int(math.sqrt(quadrant_area))     
+        pos_x = 0                                         
         for i in range(count):
             pos_y = 0
             for i in range(count):
-                self.quadrants += [((pos_x, pos_y),(pos_x + self.quadrant_size, pos_y + self.quadrant_size))]
+                self.quadrants += [Quadrant(pos_x, pos_y,pos_x + self.quadrant_size - 1, pos_y + self.quadrant_size - 1)]
                 pos_y += self.quadrant_size
             pos_x += self.quadrant_size
         self.quadrant_probabilities = np.ones((len(self.quadrants),))
@@ -91,27 +91,30 @@ class Taxi(Agent):
     def in_quadrant(self, client):
         x = client.x // self.quadrant_size
         y = client.y // self.quadrant_size
-        return x + (self.board.size[0]/self.quadrant_size) * y
+        return (x + (self.board.board_size[0]//self.quadrant_size) * y)
     
     def find_path(self, goal_x, goal_y):
         self.path = a_star.path_find(self.x, self.y, goal_x,  goal_y, self.board.board_size[0])
     
     def decide_waiting_spot(self):
         chosen_quadrant = np.random.choice(self.quadrants, p = (self.quadrant_probabilities/self.quadrant_probabilities.sum()))
-        x = rand.randrange(chosen_quadrant[0][0], chosen_quadrant[1][0])
-        y = rand.randrange(chosen_quadrant[0][1], chosen_quadrant[1][1])
+        print(chosen_quadrant)
+        x = rand.randrange(chosen_quadrant.x_start, chosen_quadrant.x_end)
+        y = rand.randrange(chosen_quadrant.y_start, chosen_quadrant.y_end)
+        print(x,y)
         return (x,y)
 
     def update_waiting_matrix(self, client):
-        update_value = len(self.quadrant_size)
+        update_value = len(self.quadrants)
         self.last_clients += [client]
-        self.quadrant_probabilities[self.in_quadrant(client)] += update_value
+        print(self.quadrant_probabilities)
+        self.quadrant_probabilities[(self.in_quadrant(client))] += update_value
         oldest_client = None
 
         # remove old information from probabilities 
         if len(self.last_clients) > 10:
             oldest_client = self.last_clients[0]
-            self.quadrant_probabilities[self.in_quadrant(oldest_client)] -= update_value
+            self.quadrant_probabilities[(self.in_quadrant(oldest_client))] -= update_value
             self.last_clients = self.last_clients[1:]
         
         return
@@ -193,12 +196,12 @@ class Taxi(Agent):
             if self.state == TaxiState.free:
                 if len(self.path) == 1:
                     self.move_next_pos()
-                    self.state == TaxiState.waiting
+                    self.state = TaxiState.waiting
                 elif self.path == '':
                     wait_pos = self.decide_waiting_spot()
                     self.find_path(wait_pos[0], wait_pos[1])
                 else:
-                    self.move_next_pos
+                    self.move_next_pos()
 
 class Client(Agent):
     def __init__(self, x, y, goal_x, goal_y):
@@ -211,3 +214,14 @@ class Client(Agent):
 
     def __repr__(self):
         return self.__str__()
+
+class Quadrant():
+    def __init__(self, x0, y0, x1, y1):
+        self.x_start = x0
+        self.x_end = x1
+        self.y_start = y0
+        self.y_end = y1
+
+    def __str__(self):
+        return "x between: " + str(self.x_start) + " and " + str(self.x_end) + " y between: " + str(self.y_start) + " and " + str(self.y_end)
+    
